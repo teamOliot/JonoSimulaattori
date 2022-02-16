495c718083015d4672144fa15d5c6fec34df04f8
IinaLaa <iina.laamo@gmail.com>
2022-02-16 16:11:28 +0200

    Added tests package to src and created first version of PalvelupisteTest.

---
+++ src/tests/PalvelupisteTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PalvelupisteTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	final void testPalvelupiste() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testLisaaJonoon() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testOtaJonosta() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
