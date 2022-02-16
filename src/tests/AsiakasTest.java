8dc7de3bb3b37d92f5b3e49931cff944a14bb07a
IinaLaa <iina.laamo@gmail.com>
2022-02-16 15:42:34 +0200

    Added tests package and first version of AsiakasTest.

---
+++ src/tests/AsiakasTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AsiakasTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	final void testAsiakas() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSetPoistumisaika() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetLapimenneetX() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetLapimenneetY() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testRaportti() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
