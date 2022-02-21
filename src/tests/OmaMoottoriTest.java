d5599c562ed169a5559595a1f52d3d5d7bcb25b3
KatjaDa <end.of.all89@gmail.com>
2022-02-21 11:12:29 +0200

    added OmaMoottoriTest

---
+++ src/tests/OmaMoottoriTest.java
@@ -0,0 +1,65 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OmaMoottoriTest {
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+
+	@Test
+	final void testAlustukset() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSuoritaTapahtuma() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testTulokset() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testOmaMoottori() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testRun() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testMoottori() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSetSimulointiaika() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testSetViive() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetViive() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
