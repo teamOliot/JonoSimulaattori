078852282be75478e1e5cce44b8ca9515183e3de
KatjaDa <end.of.all89@gmail.com>
2022-02-25 16:02:09 +0200

    created tableview for report

--- src/view/TietokantaRaporttiGUI.java
+++ src/view/TietokantaRaporttiGUI.java
@@ -1,36 +1,25 @@
 package view;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
-import simu.framework.Kello;
 import simu.model.TietokantaRaportti;
 
 public class TietokantaRaporttiGUI extends Application {
 
-//    private TableView<TietokantaRaportti> table = new TableView<TietokantaRaportti>();
-//    private final ObservableList<TietokantaRaportti> data =
-//            FXCollections.observableArrayList(
-//            		 new TietokantaRaportti(500.0789207815674, 15.0, 25.0, 0.10,33.0, 22.0, 
-//            				 12, 26, 17, 101.1,183.3 , 376.8, 300.1, 0.2, 0.3, 0.7, 0.6));
 
-	private static List<TietokantaRaportti> tietokantaRaportti;
+	private static List<TietokantaRaportti> tietokantaRaportti = new ArrayList<TietokantaRaportti>();
+	TietokantaRaportti raportti = new TietokantaRaportti(500.0789207815674, 15.0, 25.0, 0.10,33.0, 22.0, 
+            				 12, 26, 17, 101.1,183.3 , 376.8, 300.1, 0.2, 0.3, 0.7, 0.6);
 
 	public static void main(String[] args) {
 		launch(args);
@@ -55,20 +44,68 @@ public class TietokantaRaporttiGUI extends Application {
 		cl5.setCellValueFactory(new PropertyValueFactory<>("yParam"));
 		TableColumn<Double, TietokantaRaportti> cl6 = new TableColumn<>("Simulaation suoritusteho");
 		cl6.setCellValueFactory(new PropertyValueFactory<>("simulaationSuoritusteho"));
+		cl6.setMinWidth(150);
 		TableColumn<Double, TietokantaRaportti> cl7 = new TableColumn<>("Saapuneet X asiakkaat");
 		cl7.setCellValueFactory(new PropertyValueFactory<>("asiakasMaaraX"));
+		cl7.setMinWidth(150);
 		TableColumn<Double, TietokantaRaportti> cl8 = new TableColumn<>("Saapuneet Y asiakkaat");
 		cl8.setCellValueFactory(new PropertyValueFactory<>("asiakasMaaraY"));
-		TableColumn<Double, TietokantaRaportti> cl9 = new TableColumn<>("X asiakkaiden läpimenoaika");
+		cl8.setMinWidth(150);
+		TableColumn<Double, TietokantaRaportti> cl9 = new TableColumn<>("X as läpimenoaika");
 		cl9.setCellValueFactory(new PropertyValueFactory<>("xAsiakkaidenLapimenoaika"));
-		TableColumn<Double, TietokantaRaportti> cl10 = new TableColumn<>("Y asiakkaiden läpimenoaika");
+		cl9.setMinWidth(120);
+		TableColumn<Double, TietokantaRaportti> cl10 = new TableColumn<>("Y as läpimenoaika");
 		cl10.setCellValueFactory(new PropertyValueFactory<>("yAsiakkaidenLapimenoaika"));
-		
+		cl10.setMinWidth(120);
+		TableColumn<Double, TietokantaRaportti> cl11 = new TableColumn<>("As läpimenoaika yht");
+		cl11.setCellValueFactory(new PropertyValueFactory<>("asiakkaidenLapimenoaikaYht"));
+		cl11.setMinWidth(150);
+		TableColumn<Double, TietokantaRaportti> cl13 = new TableColumn<>("PP1 oleskeluaika");
+		cl13.setCellValueFactory(new PropertyValueFactory<>("oleskeluaikaPP1"));
+		cl13.setMinWidth(120);
+		TableColumn<Double, TietokantaRaportti> cl14 = new TableColumn<>("PP2 oleskeluaika");
+		cl14.setCellValueFactory(new PropertyValueFactory<>("oleskeluaikaPP2"));
+		cl14.setMinWidth(120);
+		TableColumn<Double, TietokantaRaportti> cl15 = new TableColumn<>("PP3 oleskeluaika");
+		cl15.setCellValueFactory(new PropertyValueFactory<>("oleskeluaikaPP3"));
+		cl15.setMinWidth(120);
+		TableColumn<Double, TietokantaRaportti> cl16 = new TableColumn<>("PP4 oleskeluaika");
+		cl16.setCellValueFactory(new PropertyValueFactory<>("oleskeluaikaPP4"));
+		cl16.setMinWidth(120);
+		TableColumn<Double, TietokantaRaportti> cl17 = new TableColumn<>("PP1 jonon pituus");
+		cl17.setCellValueFactory(new PropertyValueFactory<>("jononPituusPP1"));
+		cl17.setMinWidth(120);
+		TableColumn<Double, TietokantaRaportti> cl18 = new TableColumn<>("PP2 jonon pituus");
+		cl18.setCellValueFactory(new PropertyValueFactory<>("jononPituusPP2"));
+		cl18.setMinWidth(120);
+		TableColumn<Double, TietokantaRaportti> cl19 = new TableColumn<>("PP3 jonon pituus");
+		cl19.setCellValueFactory(new PropertyValueFactory<>("jononPituusPP3"));
+		cl19.setMinWidth(120);
+		TableColumn<Double, TietokantaRaportti> cl20 = new TableColumn<>("PP4 jonon pituus");
+		cl20.setCellValueFactory(new PropertyValueFactory<>("jononPituusPP4"));
+		cl20.setMinWidth(120);
 		
 		//tbv.getColumns().add(cl1);
 		tbv.getColumns().add(cl2);
 		tbv.getColumns().add(cl3);
-
+		tbv.getColumns().add(cl4);
+		tbv.getColumns().add(cl5);
+		tbv.getColumns().add(cl6);
+		tbv.getColumns().add(cl7);
+		tbv.getColumns().add(cl8);
+		tbv.getColumns().add(cl9);
+		tbv.getColumns().add(cl10);
+		tbv.getColumns().add(cl11);
+		tbv.getColumns().add(cl13);
+		tbv.getColumns().add(cl14);
+		tbv.getColumns().add(cl15);
+		tbv.getColumns().add(cl16);
+		tbv.getColumns().add(cl17);
+		tbv.getColumns().add(cl18);
+		tbv.getColumns().add(cl19);
+		tbv.getColumns().add(cl20);
+		
+		tietokantaRaportti.add(raportti);
 		for (TietokantaRaportti dbList : tietokantaRaportti) {
 			tbv.getItems().add(dbList);
 		}
